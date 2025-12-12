a0e6d988a88d2f6d8274fc3e53f4a61f95edd6eb
vitaly <vitaly.logo@gmail.com>
2025-12-12 16:04:39 +0200

    remove HW8 submodule

--- HW8/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js'],
-    testMatch: ['**/?(*.)+(spec|test).ts'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-}
